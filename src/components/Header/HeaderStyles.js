103e2fc645427f14386f0a133113ca5e5203be25
Alejandro <acastillocampillo@gmail.com>
2021-09-30 11:47:27 +0200

    Personal portfolio scafolding

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -1,7 +1,7 @@
 import { IoIosArrowDropdown } from 'react-icons/io';
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.div `
   display: grid;
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 1fr;
@@ -17,7 +17,12 @@ export const Container = styled.div`
     grid-row-gap: 0.5rem;
   }
 `;
-export const Div1 = styled.div`
+
+export const Span = styled.span `
+  font-size: 2rem;
+`;
+
+export const Div1 = styled.div `
   grid-area: 1 / 1 / 2 / 2;
   display: flex;
   flex-direction: row;
@@ -26,7 +31,7 @@ export const Div1 = styled.div`
     grid-area: 1 / 1 / 2 / 3;
   }
 `;
-export const Div2 = styled.div`
+export const Div2 = styled.div `
   grid-area: 1 / 2 / 2 / 4;
   display: flex;
   justify-content: space-around;
@@ -34,7 +39,7 @@ export const Div2 = styled.div`
     grid-area: 2 / 2 / 3 / 5;
   }
 `;
-export const Div3 = styled.div`
+export const Div3 = styled.div `
   grid-area: 1 / 5 / 2 / 6;
   display: flex;
   justify-content: space-around;
@@ -46,7 +51,7 @@ export const Div3 = styled.div`
 `;
 
 // Navigation Links
-export const NavLink = styled.a`
+export const NavLink = styled.a `
   font-size: 2rem;
   line-height: 32px;
   color: rgba(255, 255, 255, 0.75);
@@ -62,7 +67,7 @@ export const NavLink = styled.a`
 `;
 
 /// DropDown Contact
-export const ContactDropDown = styled.button`
+export const ContactDropDown = styled.button `
   border: none;
   display: flex;
   position: relative;
@@ -89,7 +94,8 @@ export const ContactDropDown = styled.button`
   }
 `;
 
-export const NavProductsIcon = styled(IoIosArrowDropdown)`
+export const NavProductsIcon = styled(IoIosArrowDropdown)
+`
   margin-left: 8px;
   display: flex;
   align-self: center;
@@ -110,7 +116,7 @@ export const NavProductsIcon = styled(IoIosArrowDropdown)`
 
 // Social Icons 
 
-export const SocialIcons = styled.a`
+export const SocialIcons = styled.a `
 transition: 0.3s ease;
 color: white;
 border-radius: 50px;
